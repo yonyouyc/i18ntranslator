f38039ca8d092b5c98d9d432d332a9ad7ae6d9e1
windknow <songhlc@yonyou.com>
2018-07-05 11:48:36 +0800

    执行翻译

--- src/index.js
+++ src/index.js
@@ -17,15 +17,6 @@ readFileContent('../code', function (destDictionary) {
   console.log('wiret')
   writeXlsx(destDictionary, '../dest/chinese.xlsx');
 })
-var path = '../dest/translated.xlsx'
-var translate = {}
-var json = readXlsx(path)
-var json = json.sort(function (a, b) {
-  return a.key.length > a.key.length
-})
-json.forEach(function (item) {
-  translate[item.key] = item.value
-})
 ///分为*.js 和 *.html
 
 // 输出所有中文内容（正则）
